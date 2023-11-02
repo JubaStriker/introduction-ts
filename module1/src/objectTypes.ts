aea8fa4013ca0eeb552720dd4996aa3a02bfa80a
JubaStriker <jubastriker@gmail.com>
2023-11-02 11:55:29 +0600

    Object type covered

---
+++ module1/src/objectTypes.ts
@@ -0,0 +1,14 @@
+// Reference type --> Object
+
+
+const user: {
+    company: "Programming Hero";     // type -> literal type
+    firstName: string;               //Using semicolon in type declaration
+    middleName?: string;             // '?' = Optional type
+    lastName: string;
+} = {
+    company: "Programming Hero",
+    firstName: 'John',
+
+    lastName: 'Smith'
+}
