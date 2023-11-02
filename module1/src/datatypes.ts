fc03dffe1d116ac4031e49ccce7591e7ce209992
JubaStriker <jubastriker@gmail.com>
2023-11-02 11:47:28 +0600

    Basic types covered

---
+++ module1/src/datatypes.ts
@@ -0,0 +1,28 @@
+// Basic data types
+
+//string
+const firstName: string = 'Jubair'
+
+//number
+const roll: number = 2002276
+
+//boolean
+const isAdmin: boolean = false
+
+//undefined
+const x: undefined = undefined
+
+//null
+const y: null = null
+
+//array
+let friends: string[] = ['Mahi', 'Arpan', 'Mahim']
+
+let eligibleRolls: number[] = [2002245, 2002246, 2002276]
+
+
+//tuple --> array --> order --> type of values
+
+let coordinates: [number, number] = [12, 19]
+
+let nameAge: [string, number, boolean] = ["Jubair", 23, true]
