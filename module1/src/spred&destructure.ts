e0037521a3ffbb0a12f7eb621c8aed394fa8d88a
JubaStriker <jubastriker@gmail.com>
2023-11-03 10:51:43 +0600

    Type alias covered

--- module1/src/spred&destructure.ts
+++ module1/src/spred&destructure.ts
@@ -5,6 +5,42 @@
     //Spread operator and destructuring
 
 
+    const bros1: string[] = [
+        "Mahi",
+        "Arpan",
+        "Mahim"
+    ];
+
+    const bros2: string[] = [
+        "Rimi",
+        "Faiza",
+        "Dristy"
+    ];
+
+    // bros1.push(bros2);            //Cannot assign array in string
+    bros1.push(...bros2);         // ... (Spread operator) spreads the elements of the array
+
+    const devs1 = {
+        pro: "Niam",
+        mid: 'Sunjid',
+        noob: 'Shoaib'
+    }
+    const devs2 = {
+        pro: "Arpan",
+        mid: 'Tonmoy',
+        noob: 'Nayeem'
+    }
+
+    const devs = {
+        ...devs1,
+        ...devs2,
+    }
+
+    console.log(devs)
+
+
+
+
 
 
 
---
+++ module1/src/typeAlias.ts
@@ -0,0 +1,33 @@
+type Student = {
+    name: string;
+    roll: number;
+    address: string;
+    department: string;
+    isGraduated: boolean;
+}
+
+const student1: Student = {
+    name: 'Jubair',
+    roll: 1,
+    address: 'DHK',
+    department: 'EEE',
+    isGraduated: false
+}
+const student2: Student = {
+    name: 'Mahi',
+    roll: 2,
+    address: 'DHK',
+    department: 'EEE',
+    isGraduated: false
+}
+
+
+const student3: Student = {
+    name: 'Mahim',
+    roll: 2,
+    address: 'DHK',
+    department: 'EEE',
+    isGraduated: true
+}
+
+
