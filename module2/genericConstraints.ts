e60e33ca49716a18df86b202ebb7b11893b7c62d
JubaStriker <jubastriker@gmail.com>
2023-11-06 13:39:12 +0600

    Generic contstraints covered

--- module2/genericConstraints.ts
+++ module2/genericConstraints.ts
@@ -1,3 +1,33 @@
 {
     // Generic constraints
+    type Vehicle = {
+        bike: string;
+        car: string;
+        ship: string;
+    }
+
+    type Owner = 'bike' | 'car' | 'ship';
+
+    type Owner2 = keyof Vehicle;
+
+    const owner2: Owner2 = 'car'
+
+    const getPropertyValue = <X, Y extends keyof X>(object: X, key: Y) => {
+        return object[key];
+    }
+
+    const user = {
+        name: 'Jubair',
+        email: 'jubair@gmail.com',
+        roll: 2002267
+    }
+
+    const car = {
+        brand: 'Toyota',
+        model: 'Supra'
+
+    }
+
+    const result = getPropertyValue(car, 'model')
+    console.log(result)
 }
